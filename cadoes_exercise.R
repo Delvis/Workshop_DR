f67033a6fb5033e4031ec904f75055e13121e8b2
João Coelho <joaopedrocoelho@gmail.com>
2019-03-15 10:30:03 +0000

    final version

--- cadoes_exercise.R
+++ cadoes_exercise.R
@@ -1,29 +1,29 @@
 # Can we achieve a perfect solution for CADOES?
 
 cad <- readRDS("./workshop/cadoes.rds") # dataset with measurements from pelvic bones
-dim(cad)
-names(cad) # your can use scr() or summary() to have a better look at the data
-cadoes <- na.omit(cad[ , -c(1:2)]) # only the numbers (vertebral heights)
-str(cad)
 
-# t-SNE 
+# Exercise 1: What are the dimensions of cad?
 
-cadT <- Rtsne(cadoes, dims = 2, verbose = TRUE, max_iter = 500)
+# Exercise 2: Explore the general structure of the data
+
+# Exercise 3: Remove the categorical variables.
+
+
+# Exercise 4: Calculate a t-SNE and visualize it
+
+cadT <- Rtsne(?, dims = ?, verbose = ?, max_iter = 500)
 # visualizing
-vizier::embed_plot(cadT$Y, cad$SEX,
-                   title = "t-sne 2D mapping of CADOES")
+# DO A PLOT OF cadT here
 
-# UMAP
+# Exercise 5: Calculate a UMAP and visualize it
 
-cadU <- umap(cadoes, scale = TRUE, verbose = TRUE)
-vizier::embed_plot(cadU, cad$SEX,
-                   title = "UMAP 2D mapping of CADOES (unsupervised)")
+cadU <- umap(?, scale = ?, verbose = ?)
+vizier::embed_plot(?, ?, title = ?)
 
-# UMAP with a Y-target
+# Exercise 6: Calculate a supervised umap and visualize it
 
-cadS <- umap(cadoes, scale = TRUE, verbose = TRUE, y = cad$SEX)
-vizier::embed_plot(cadS, cad$SEX,
-                   title = "UMAP 2D mapping of CADOES (semi-supervised)")
+cadS <- umap()
+vizier::embed_plot(cadS, ?, title = "UMAP 2D mapping of CADOES (semi-supervised)")
 
 # But does this always work?
 
@@ -34,8 +34,8 @@ smp_size <- floor(0.75 * nrow(cad))
 set.seed(1992)
 train_ind <- sample(seq_len(nrow(cad)), size = smp_size)
 
-train <- cad[train_ind, ]
-test <- cad[-train_ind, ]
+# Exercise 7: Separate your dataset in "train" and a "test" set
+
 set.seed(1992)
 cadTRAIN <- umap(train[,3:40], scale = TRUE, verbose = TRUE, y = train$SEX, ret_model = TRUE)
 vizier::embed_plot(cadTRAIN$embedding, train$SEX,
@@ -51,6 +51,6 @@ cadTEST <- umap_transform(test[,3:40], cadTRAIN)
 vizier::embed_plot(cadTEST, test$SEX,
                    title = "UMAP 2D mapping of CADOES (semi-supervised)")
 
-6/nrow(test) # Accuracy close to 91%
+# Exercise 8: Estimate error
 
 # No free lunch theorem...
